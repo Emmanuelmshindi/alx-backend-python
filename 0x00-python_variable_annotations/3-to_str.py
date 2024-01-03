83cea3599e4f13872a1b9783e417b98430ce17cf
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-01-03 06:58:06 +0000

    Task 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Defines a function to_str"""
+
+
+def to_str(n: float) -> str:
+    """Str representation of  n
+    Args:
+        n: float
+    return str
+    """
+    return str(n)
