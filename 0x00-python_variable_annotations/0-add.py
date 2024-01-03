1d53893ca3921e8800a08739c987f594f7cf0e5a
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-01-03 05:31:41 +0000

    Initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defines a function add"""
+
+
+def add(a: float, b: float) -> float:
+    """Add a and b and return sum
+    Args:
+        a: float
+        b: float
+    return sum(float)
+    """
+    return a + b
