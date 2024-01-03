fc1050dd8a22da63efcdc6ca18714efa1c84e670
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-01-03 05:59:52 +0000

    Task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defines a function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate str1 and str2
+    Args:
+        str1: string
+        str2: string
+    return str(str)
+    """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defines a function floor"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Floors n
+    Args:
+        n: float
+    return int
+    """
+    return math.floor(n)
