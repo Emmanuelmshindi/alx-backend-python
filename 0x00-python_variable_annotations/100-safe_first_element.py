865847e0e8649cb8f3713197bd6bfe093d2b58c9
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-01-04 11:17:11 +0000

    Task 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,15 @@ from typing import Sequence, Any, Union, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """
+    Function with duck-types
+
+    Parameters:
+    - lst: Sequence[Any]: List
+
+    Returns:
+    - optional
+    """
     if lst:
         return lst[0]
     else:
