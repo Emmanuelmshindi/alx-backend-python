59d1ae4ca84976d24cba336fc3a1d1d493da16cb
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-01-04 11:13:00 +0000

    Task 100

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Defines a function with Duck types"""
+
+from typing import Sequence, Any, Union, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
