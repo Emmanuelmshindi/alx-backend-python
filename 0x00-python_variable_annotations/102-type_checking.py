aea3612e0aea53035ea1eac3c4400eea92758634
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-01-04 11:54:23 +0000

    Task 102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+"""Defines zoom array function"""
 
 from typing import Tuple, List
 
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Function to return the zoomed in value in relation to the factor
+    """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
